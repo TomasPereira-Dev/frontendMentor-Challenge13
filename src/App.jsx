8dd65c916f654a0ed0dd57ad828310eb044c3dba
Tomas Pereira <tomaspereira2k@gmail.com>
2023-12-04 09:26:07 -0300

    added event handlers to input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const storage = JSON.parse(localStorage.getItem("tasks"));
+
+  const inputRef = useRef(null);
+  const [taskName, setTaskName] = useState(null);
+  const [taskList, setTaskList] = useState ([]);
+
+  const updateStorage = () => {
+    console.log(taskName)
+    const newJson = JSON.stringify(taskList);
+    localStorage.setItem("tasks", newJson)
+  }
+  
+  const taskHandler = (e) => {
+    if(e.key === "Enter" && taskName !== null){
+      const taskObject = {
+                            name: taskName,
+                            isActive: true,                      
+      }
+      setTaskList([...taskList, taskObject])
+      setTaskName(null);
+      inputRef.current.value = " ";
+      updateStorage();
+    }
+  }
 
   return (
     <>
+    <div className='flex flex-col items-center py-12 bg-darkmode1 bg-no-repeat'>
+      <div className='flex flex-col'>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-2xl text-white tracking-letterSpacing1 font-bold'>TODO</h1>
+          <img className='relative -top-0.5' src="./src/assets/icon-sun.svg" alt=" "/>
+        </div>
+        <div className='flex gap-6 px-6 py-2 mt-6 bg-gray-500 rounded'>
+          <div className='p-3 border rounded-full'>
+           {/* <img src="./src/assets/icon-check.svg" alt=" "/>*/}
+          </div>
+          <input className='text-lg bg-transparent' type="text" placeholder='Create a new todo...' ref={inputRef} onChange={(e)=>{setTaskName(e.target.value)}} onKeyDown={(e)=>{taskHandler(e)}}/>
+        </div>
+      </div>
+    </div>
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/*.jsx"
+  ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+          darkmode1: "url('./src/assets/bg-mobile-dark.jpg')"
+        },
+        backgroundColor: {
+          darkBlue1: "hsl(235, 21%, 11%)",
+          darkBlue2: "hsl(235, 24%, 19%)",
+          lightGrayishBlue1: "hsl(234, 39%, 85%)",
+          lightGrayishBlue2: "hsl(236, 33%, 92%)",
+          darkGrayishBlue1: "hsl(234, 11%, 52%)",
+          darkGrayishBlue2: "hsl(233, 14%, 35%)",
+          darkGrayishBlue3: "hsl(237, 14%, 26%)"
+
+        },
+        fontFamily: {
+          sans: "'Josefin Sans', sans-serif"
+        },
+        letterSpacing: {
+          letterSpacing1: "1ch"
+        }
+    },
   },
   plugins: [],
 }
