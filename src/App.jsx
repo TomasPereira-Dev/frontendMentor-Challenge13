1526bdb2d1164cc459bc63cf96f9242d8852ee5d
Tomas Pereira <tomaspereira2k@gmail.com>
2023-12-08 14:55:38 -0300

    finish complete layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { nanoid } from 'nanoid';
 import './App.css'
 
@@ -14,24 +14,16 @@ function App() {
   }
 
   const inputRef = useRef(null);
+  const checkRef = useRef(null);
   const [taskName, setTaskName] = useState(null);
   const [taskList, setTaskList] = useState(initialFunction);
-  const mappedTaskList = taskList.map(item => <li className='flex justify-between px-6 py-3' key={item.id}>
-                                                <div className='flex gap-4 items-center'>
-                                                  <button className='p-2 border border-darkGrayishBlue2 rounded-full'>
-                                                    <img className='invisible' src="./src/assets/icon-check.svg" alt=" "/>
-                                                  </button>
-                                                  <p className='text-lightGrayishBlue1'>{item.name}</p>
-                                                </div>
-                                                <button>
-                                                  <img src="./src/assets/icon-cross.svg" alt=" "/>
-                                                </button>
-                                              </li>);
+  const [switcherIcon, setSwitcherIcon] = useState("./src/assets/icon-sun.svg");
+  const newJson = JSON.stringify(taskList);
 
-  const updateStorage = (newJson) => {
+  const updateStorage = useCallback((newJson => {
     localStorage.setItem("tasks", newJson);
-  }
-  
+  }), [])
+
   const taskHandler = (e) => {
     if(e.key === "Enter" && taskName !== null){
       const taskObject = {
@@ -45,32 +37,65 @@ function App() {
     }
   }
 
-  useEffect(()=>{
-    const newJson = JSON.stringify(taskList);  
+  const checkHandler = () => {
+     checkRef.current.classList.toggle("line-through")
+  }
+
+  const darkModeHandler = () => {
+    const htmlElement = document.querySelector("html");
+    htmlElement.classList.toggle("dark");
+    if(htmlElement.classList.contains("dark")){
+      setSwitcherIcon("./src/assets/icon-moon.svg");
+    }else{
+      setSwitcherIcon("./src/assets/icon-sun.svg");
+    }
+  }
+
+  const mappedTaskList = taskList.map(item =>
+    <li className='flex justify-between px-6 py-3' key={item.id}>
+      <div className='flex gap-4 items-center'>
+        <button className='p-2 border dark:border-darkGrayishBlue2 rounded-full' onClick={checkHandler}>
+          <img className='invisible' src="./src/assets/icon-check.svg" alt=" "/>
+        </button>
+        <p className='text-darkGrayishBlue4 dark:text-lightGrayishBlue1' ref={checkRef}>{item.name}</p>
+      </div>
+      <button>
+        <img src="./src/assets/icon-cross.svg" alt=" "/>
+      </button>
+    </li>);
+
+  useEffect(() => {  
     updateStorage(newJson)
-  })
+  },[updateStorage, newJson])
 
   return (
     <>
       <div className='relative py-12 w-10/12 md:w-6/12'>
         <div className='flex justify-between items-center mb-8'>
           <h1 className='text-2xl text-white tracking-letterSpacing1 font-bold'>TODO</h1>
-          <img className='relative -top-0.5' src="./src/assets/icon-sun.svg" alt=" "/>
+          <button onClick={darkModeHandler}>
+            <img className='relative -top-0.5' src={switcherIcon} alt= " "/>
+          </button>
         </div>
-        <div className='flex gap-4 px-6 py-3 bg-darkGrayishBlue3 rounded'> 
-          <button className='p-2 border border-darkGrayishBlue2 rounded-full'>
+        <div className='flex gap-4 px-6 py-3 bg-lightGray1 dark:bg-darkGrayishBlue3 rounded'> 
+          <button className='p-2 border dark:border-darkGrayishBlue2 rounded-full'>
               <img className='invisible' src="./src/assets/icon-check.svg" alt=" "/>
           </button>
-          <input className='text-lg text-lightGrayishBlue1 bg-transparent' type="text" placeholder='Create a new todo...' ref={inputRef} onChange={(e)=>{setTaskName(e.target.value)}} onKeyDown={(e)=>{taskHandler(e)}}/>
+          <input className='relative top-0.5 w-full text-lg text-darkGrayishBlue4 dark:text-lightGrayishBlue1 bg-transparent outline-none caret-brightBlue caret' type="text" placeholder='Create a new todo...' ref={inputRef} onChange={(e)=>{setTaskName(e.target.value)}} onKeyDown={(e)=>{taskHandler(e)}}/>
         </div>
         <div className='mt-4'>
           <div className='absolute flex flex-col justify-between w-full -bottom-100'>
-            <ul className='rounded-t divide-y divide-darkGrayishBlue2 bg-darkGrayishBlue3'>{mappedTaskList}</ul>
-            <div className='flex justify-between px-6 py-3 text-darkGrayishBlue1 bg-darkGrayishBlue3 border-t border-t-darkGrayishBlue2 rounded-b'>
+            <ul className='bg-lightGray1 dark:bg-darkGrayishBlue3 divide-y dark:divide-darkGrayishBlue2 rounded-t'>{mappedTaskList}</ul>
+            <div className='flex justify-between px-6 py-3 text-darkGrayishBlue1 bg-lightGray1 dark:bg-darkGrayishBlue3 border-t dark:border-t-darkGrayishBlue2 rounded-b'>
               <p>items left</p>
+              <div className='hidden md:flex md:gap-2'>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+              </div>
               <button>Clear Completed</button>              
             </div>
-            <div className='flex justify-center gap-4 px-6 py-3 mt-4 text-darkGrayishBlue1 font-bold bg-darkGrayishBlue3 rounded'>
+            <div className='flex justify-center gap-4 px-6 py-3 mt-4 text-darkGrayishBlue1 font-bold bg-lightGray1 dark:bg-darkGrayishBlue3 rounded md:hidden'>
               <button>All</button>
               <button>Active</button>
               <button>Completed</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,23 @@ export default {
   theme: {
     extend: {
         backgroundImage: {
-          darkmode1: "url('./src/assets/bg-mobile-dark.jpg')"
+          darkmode1: "url('./src/assets/bg-mobile-dark.jpg')",
+          darkmode2: "url('./src/assets/bg-desktop-dark.jpg')",
+          lightmode1: "url('./src/assets/bg-mobile-light.jpg')",
+          lightmode2: "url('./src/assets/bg-desktop-light.jpg')"
         },
         backgroundColor: {
           darkBlue1: "hsl(235, 21%, 11%)",
           darkBlue2: "hsl(235, 24%, 19%)",
           lightGrayishBlue1: "hsl(234, 39%, 85%)",
           lightGrayishBlue2: "hsl(236, 33%, 92%)",
+          lightGrayishBlue3: "hsl(236, 33%, 92%)",
+          lightGrayishBlue4: "hsl(233, 11%, 84%)",
           darkGrayishBlue1: "hsl(234, 11%, 52%)",
           darkGrayishBlue2: "hsl(233, 14%, 35%)",
-          darkGrayishBlue3: "hsl(237, 14%, 26%)"
+          darkGrayishBlue3: "hsl(237, 14%, 26%)",
+          lightGray1: "hsl(0, 0%, 98%)",
+
 
         },
         fontFamily: {
@@ -26,7 +33,9 @@ export default {
           lightGrayishBlue1: "hsl(234, 39%, 85%)",
           lightGrayishBlue2: "hsl(236, 33%, 92%)",
           darkGrayishBlue1: "hsl(234, 11%, 52%)",
-          darkGrayishBlue2: "hsl(233, 14%, 35%)"
+          darkGrayishBlue2: "hsl(233, 14%, 35%)",
+          darkGrayishBlue3: "hsl(236, 9%, 61%)",
+          darkGrayishBlue4: "hsl(235, 19%, 35%)"
         },
         letterSpacing: {
           letterSpacing1: "1ch"
@@ -34,9 +43,14 @@ export default {
         borderColor: {
           darkGrayishBlue1: "hsl(234, 11%, 52%)",
           darkGrayishBlue2: "hsl(233, 14%, 35%)"
+        },
+        caretColor: {
+          brightBlue: "hsl(220, 98%, 61%)",
+
         }
-    },
+    }
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: []
 }
 
