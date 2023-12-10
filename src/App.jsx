8902163c371801a2997a6ed02502975346111d1a
Tomas Pereira <tomaspereira2k@gmail.com>
2023-12-10 17:24:19 -0300

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,10 @@ function App() {
   const inputRef = useRef(null);
   const [taskName, setTaskName] = useState(null);
   const [taskList, setTaskList] = useState(initialFunction);
+  const [filteredTaskList, setFilteredTaskList] = useState([]);
   const [switcherIcon, setSwitcherIcon] = useState("./src/assets/icon-sun.svg");
   const [listCounter, setListCounter] = useState(0);
+  const [listFilter, setListFilter] = useState("all");
   const newJson = JSON.stringify(taskList);
 
   const updateStorage = useCallback((newJson => {
@@ -81,6 +83,29 @@ function App() {
     setListCounter(counter)
   }, [taskList])
 
+  const filterHandler = useCallback(() => {
+    const taskListCopy = taskList.slice();
+    if(listFilter === true){
+      for(let i = 0; i < taskListCopy.length; i++){
+        console.log(taskListCopy[i].isActive)
+        if(taskListCopy[i].isActive === false){
+            taskListCopy.splice(i, 1);
+            console.log(taskListCopy[i].isActive)
+        }
+      }
+      setFilteredTaskList(taskListCopy);   
+    } 
+    if(listFilter === false){
+      for(let i = 0; i < taskListCopy.length; i++){
+        console.log(taskListCopy[i].isActive)
+        if(taskListCopy[i].isActive === true){
+          taskListCopy.splice(i, 1);
+        }
+      }
+      setFilteredTaskList(taskListCopy);
+    }
+  },[taskList, listFilter])
+ 
   const mappedTaskList = taskList.map(item => 
     <li className={`flex justify-between px-6 py-3 ${item.isActive ? `no-underline` : `line-through`}`} key={item.id}>
       <div className='flex gap-4 items-center'>
@@ -94,10 +119,26 @@ function App() {
       </button>
     </li>);
 
+    const mappedFilteredList = filteredTaskList.map(item => 
+      <li className={`flex justify-between px-6 py-3 ${item.isActive ? `no-underline` : `line-through`}`} key={item.id}>
+      <div className='flex gap-4 items-center'>
+        <button className='p-2 border dark:border-darkGrayishBlue2 rounded-full' onClick={() => checkHandler(item.id)}>
+          <img className='invisible' src="./src/assets/icon-check.svg" alt=" "/>
+        </button>
+        <p className={`text-darkGrayishBlue4 dark:text-lightGrayishBlue1`}>{item.name}</p>
+      </div>
+      <button onClick={() => {deleteHandler(item.id)}}>
+        <img src="./src/assets/icon-cross.svg" alt=" "/>
+      </button>
+    </li>);
+    
+
   useEffect(() => {  
     updateStorage(newJson)
     counterHandler()
-  },[counterHandler ,updateStorage, newJson])
+    filterHandler()
+    
+  },[filterHandler, counterHandler, updateStorage, newJson])
 
   return (
     <>
@@ -116,20 +157,20 @@ function App() {
         </div>
         <div className='mt-4'>
           <div className='absolute flex flex-col justify-between w-full -bottom-100'>
-            <ul className='bg-lightGray1 dark:bg-darkGrayishBlue3 divide-y dark:divide-darkGrayishBlue2 rounded-t'>{mappedTaskList}</ul>
+            <ul className='bg-lightGray1 dark:bg-darkGrayishBlue3 divide-y dark:divide-darkGrayishBlue2 rounded-t'>{listFilter === "all" ? mappedTaskList : mappedFilteredList}</ul>
             <div className='flex justify-between px-6 py-3 text-darkGrayishBlue1 bg-lightGray1 dark:bg-darkGrayishBlue3 border-t dark:border-t-darkGrayishBlue2 rounded-b'>
               <p>{`${listCounter} items left`}</p>
               <div className='hidden md:flex md:gap-2'>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => setListFilter("all")}>All</button>
+                <button onClick={() => setListFilter(true)}>Active</button>
+                <button onClick={() => setListFilter(false)}>Completed</button>
               </div>
               <button>Clear Completed</button>              
             </div>
             <div className='flex justify-center gap-4 px-6 py-3 mt-4 text-darkGrayishBlue1 font-bold bg-lightGray1 dark:bg-darkGrayishBlue3 rounded md:hidden'>
-              <button>All</button>
-              <button>Active</button>
-              <button>Completed</button>
+              <button onClick={() => setListFilter("all")}>All</button>
+              <button onClick={() => setListFilter(true)}>Active</button>
+              <button onClick={() => setListFilter(false)}>Completed</button>
             </div>
           </div>
         </div>
